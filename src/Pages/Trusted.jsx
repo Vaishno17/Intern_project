97a42c3aeeba0c5b1d7f8b2366ead81258eb5ca6
Vaishnavi Matsagar <100572533+Vaishno17@users.noreply.github.com>
2024-08-10 22:19:03 +0530

    Update Trusted.jsx

--- src/Pages/Trusted.jsx
+++ src/Pages/Trusted.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Trusted = () => {
   return (
     <div>
-      Trusted
+      <h1> Trusted </h1>
+      <p>
+        Global Reach: <br />
+        With a strong presence in multiple countries, we are dedicated to making a positive impact on global health. Our products are trusted by healthcare professionals and patients around the world, and we continue to expand our reach to bring our solutions to more communities.
+      </p>
     </div>
   )
 }
